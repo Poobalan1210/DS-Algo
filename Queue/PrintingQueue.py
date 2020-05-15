64cf91c05b0f1b373696f9cbecc51294db091c37
thesparkvision <aman15.pandya2gmail.com>
2020-05-15 21:34:46 +0530

    Modified PrintingQueue program , included a simulation function for running the simulation

--- Queue/PrintingQueue.py
+++ Queue/PrintingQueue.py
@@ -1,4 +1,4 @@
-from queue import queue
+from queue import Queue
 
 class Printer:
 	def __init__(self,ppm):
@@ -18,7 +18,7 @@ class Printer:
 		else:
 			return False
 
-	def newTask(self,new_task):
+	def startNext(self,new_task):
 		self.currentTask=new_task
 		self.timeRemaining=new_task.getPages()*60/self.page_rate
 
@@ -38,3 +38,34 @@ class Task:
 	def waitTime(self,current_time):
 		return current_time-self.timestamp
 
+def simulation(numSeconds,pages_per_minute):
+	lab_printer=Printer(pages_per_minute)
+	printing_queue=Queue()
+	waiting_times=[]
+
+	for current_second in range(numSeconds):
+		
+		if newPrintTask():
+			#print("I am in")
+			task=Task(current_second)
+			printing_queue.enqueue(task)
+		
+		if((not lab_printer.busy()) and (not printing_queue.isEmpty())):
+			next_task=printing_queue.dequeue()
+			waiting_times.append(next_task.waitTime(current_second))
+			lab_printer.startNext(next_task)
+		lab_printer.tick()
+
+	averageWait=sum(waiting_times)/(len(waiting_times))
+	print("Average Wait %6.2f secs %3d tasks remaining."%(averageWait,printing_queue.size()))
+
+def newPrintTask():
+	num=random.randrange(1,181)
+	if num==180:
+		return True
+	else:
+		return False
+
+if __name__=="__main__":
+	for i in range(10):
+		simulation(3600,10)
