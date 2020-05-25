84709c601ba0abe11bbd3ef85a516c1a12b2eb24
thesparkvision <aman15.pandya@gmail.com>
2020-05-25 19:08:39 +0530

    added new method prepend on linkedlist and changed insertLast method name to append

--- LinkedList/CircularLinkedList.py
+++ LinkedList/CircularLinkedList.py
@@ -7,7 +7,7 @@ class CircularLinkedList:
 	def __init__(self):
 		self.head=None
 
-	def insertLast(self,new_data):
+	def append(self,new_data):
 		new_node=Node(new_data)
 		if self.head==None:
 			self.head=new_node
@@ -30,11 +30,24 @@ class CircularLinkedList:
 				if curr==self.head:
 					break
 
+	def prepend(self,new_data):
+		new_node=Node(new_data)
+		if self.head==None:
+			self.head=new_node
+			new_node.next=self.head
+		else:
+			curr=self.head
+			while curr.next!=self.head:
+				curr=curr.next
+			curr.next=new_node
+			new_node.next=self.head
+			self.head=new_node
+
 if __name__=="__main__":
 	CLL=CircularLinkedList()
-	CLL.insertLast(1)
-	CLL.insertLast(2)
-	CLL.insertLast(3)
-	CLL.insertLast(4)
-	CLL.insertLast(5)
+	CLL.prepend(1)
+	CLL.prepend(2)
+	CLL.prepend(3)
+	CLL.prepend(4)
+	CLL.prepend(5)
 	CLL.traverse()
