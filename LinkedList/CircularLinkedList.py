426916513cedfa25f501227d1ab12e9af5bb7972
thesparkvision <aman15.pandya@gmail.com>
2020-05-25 20:47:03 +0530

    added josephus problem method and is_circular_linked_list method in the CircularLinkedList module

--- LinkedList/CircularLinkedList.py
+++ LinkedList/CircularLinkedList.py
@@ -88,6 +88,36 @@ class CircularLinkedList:
 			else:
 				prev.next=curr.next
 
+	def remove_node(self,deletion_node):
+		if self.head==None:
+			print("CircularLinkedList empty")
+		else:
+			#Logic to check where deletion key is present
+			curr=self.head
+			prev=None
+			while curr.next!=self.head:
+				if curr==deletion_node:
+					key_found=True
+					break
+				prev=curr
+				curr=curr.next
+
+			'''
+			Checking if the deletion key matches with the head node and
+			if matches delete it
+			'''
+			if prev==None:
+				if self.head==self.head.next:
+					self.head=None
+				else:
+					temp=self.head
+					while temp.next!=self.head:
+						temp=temp.next
+					temp.next=curr.next
+					self.head=temp.next
+			else:
+				prev.next=curr.next
+
 	def __len__(self):
 		if self.head==None:
 			return 0
@@ -126,13 +156,36 @@ class CircularLinkedList:
 		print("\n")
 		split_clist.traverse()
 
+	def josephus_problem(self,step):
+		curr=self.head
+
+		while len(self)>1:
+			count=1
+			while count!=step:
+				curr=curr.next
+				count+=1
+			print("KILL : "+str(curr.data))
+			next_node=curr.next
+			self.remove_node(curr)
+			curr=next_node
+
+	def is_circular_linked_list(self,input_list):
+		head=input_list.head
+		curr=input_list.head
+		while curr.next:
+			if curr.next==head:
+				return True
+			curr=curr.next
+		return False
+			
 if __name__=="__main__":
-	CLL=CircularLinkedList()
-	CLL.prepend(1)
-	CLL.prepend(2)
-	CLL.prepend(3)
-	CLL.prepend(4)
-	#CLL.prepend(5)
-	CLL.traverse()
-	print("\n")
-	CLL.split_list()
+	CLL1=CircularLinkedList()
+	CLL1.append(1)
+	CLL1.append(2)
+	CLL1.append(3)
+	CLL2=CircularLinkedList()
+	CLL2.append(4)
+	CLL2.append(5)
+	CLL2.append(6)
+	print(CLL1.is_circular_linked_list(CLL2))
+
